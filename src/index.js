6adce75e6d998f64d9c1752311240904ff645ecb
Cunningham16 <83078128+Cunningham16@users.noreply.github.com>
2022-04-20 21:51:56 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,80 +4,76 @@ let tasks = document.querySelectorAll(".task");
 let closeButtons = document.querySelectorAll(".close-button");
 let taskInput = document.querySelector(".input-text");
 let textTask = document.querySelectorAll(".text-task");
+let displayEmpty = document.querySelector(".display-empty");
 
-taskInput.addEventListener("keydown", function(e){
-	if(e.keyCode === 13 && tasks.length != 0){
-		let lastNumTask = tasks[tasks.length-1].dataset.numoftask;
-		let first = '<input type="text" value="'+taskInput.value+'" class="text-task" data-numoftask="'+(Number(lastNumTask)+1)+'">';
-		let second = '<input type="checkbox" class="checkbox-task" data-numOfTask="'+(Number(lastNumTask)+1)+'">';
-		let third = '<button class="close-button" data-numOfTask="'+(Number(lastNumTask)+1)+'">Close</button>'
-		let newTask = document.createElement("div");
-		newTask.classList.add('task');
+function renderTasks(){
+	tasks = document.querySelectorAll(".task");
+
+	if(tasks.length === 0){
+		displayEmpty.classList.remove("hidden");
+	}else if(tasks.length != 0){
+		displayEmpty.classList.add("hidden");
+	}
+}
+
+function otherfunc(){
+	for(let task of tasks){
+		let close = task.querySelector(".close-button");
+		let text = task.querySelector(".text-task");
+		let check = task.querySelector(".checkbox-task");
+		close.onclick = function(){
+			task.remove();
+			renderTasks();
+		}
+		check.onchange = function(){
+			text.style.transition = "0.2s";
+			text.classList.toggle("checked")
+		}
+		text.addEventListener("keydown", function(e){
+			if(e.keyCode === 13){
+				text.blur();
+			}
+		});		
+	}
+}
+otherfunc();
+
+function createTask(num){
+	let newTask = document.createElement("div");
+
+	newTask.classList.add('task');
+
+	if(tasks.length === 0){
+		let first = '<input type="text" value="'+taskInput.value+'" class="text-task" data-numoftask="'+1+'">';
+		let second = '<input type="checkbox" class="checkbox-task" data-numOfTask="'+1+'">';
+		let third = '<button class="close-button" data-numOfTask="'+1+'">Close</button>';
+
+		newTask.dataset.numoftask=1;
+		newTask.innerHTML = first+second+third;
+		taskList.append(newTask);
+	}else{
+		let first = '<input type="text" value="'+taskInput.value+'" class="text-task" data-numoftask="'+(num+1)+'">';
+		let second = '<input type="checkbox" class="checkbox-task" data-numOfTask="'+(num+1)+'">';
+		let third = '<button class="close-button" data-numOfTask="'+(num+1)+'">Close</button>';
+
 		newTask.dataset.numoftask=Number(tasks[tasks.length-1].dataset.numoftask)+1;
 		newTask.innerHTML = first+second+third;
 		taskList.append(newTask);
-		taskInput.value = '';
-		tasks = document.querySelectorAll(".task");
+	}
 
-		for(let task of tasks){
-			let close = task.querySelector(".close-button");
-			let check = task.querySelector(".checkbox-task");
-			let text = task.querySelector(".text-task");
-			close.onclick = function(){
-				task.remove();
-				tasks = document.querySelectorAll(".tasks");
-			}
-			check.onchange = function(){
-				text.classList.toggle("checked")
-			}
-		}
-	}else if(e.keyCode === 13 && tasks.length === 0){
-		let first = '<input type="text" value="'+taskInput.value+'" class="text-task" data-numoftask="'+1+'">';
-		let second = '<input type="checkbox" class="checkbox-task" data-numOfTask="'+1+'">';
-		let third = '<button class="close-button" data-numOfTask="'+1+'">Close</button>'
-		let newTask = document.createElement("div");
-		newTask.classList.add('task');
-		newTask.dataset.numoftask=1;
-		newTask.innerHTML = first+second+third;
-		taskInput.value = '';
-		
-		console.log(tasks);
-		taskList.append(newTask);
+	taskInput.value = '';
+}
 
-		tasks = document.querySelectorAll(".task");
-		for(let task of tasks){
-			let close = task.querySelector(".close-button");
-			let text = task.querySelector(".text-task");
-			let check = task.querySelector(".checkbox-task");
-			close.onclick = function(){
-				task.remove();
-				tasks = document.querySelectorAll(".tasks");
-			}
-			check.onchange = function(){
-				text.classList.toggle("checked")
-			}		
-		}
-	}else if(e.keyCode === 13 && taskInput.value == ""){console.log(123)}
+taskInput.addEventListener("keydown", function(e){
+	if(e.keyCode === 13 && tasks.length != 0 && taskInput.value != ''){
+		let lastNumTask = tasks[tasks.length-1].dataset.numoftask;
+
+		createTask(Number(lastNumTask));
+		renderTasks();
+		otherfunc();
+	}else if(e.keyCode === 13 && tasks.length === 0 && taskInput.value != ''){
+		createTask(0);
+		renderTasks();
+		otherfunc();
+	}
 })
-
-for(let task of tasks){
-	let close = task.querySelector(".close-button");
-	let check = task.querySelector(".checkbox-task");
-	let text = task.querySelector(".text-task");
-	close.onclick = function(){
-		task.remove();
-		tasks = document.querySelectorAll(".tasks");
-	}
-	check.onchange = function(){
-		text.classList.toggle("checked")
-	}
-}
-
-for(let text of textTask){
-	text.addEventListener("keydown", function(e){
-		if(e.keyCode === 13){
-			text.blur();
-		}
-	});
-}
-
